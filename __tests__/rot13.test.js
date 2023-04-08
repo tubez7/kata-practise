d12484f50d98e97d40f20da4b42436ffe92ee9f8
tubez7 <thescopeboys@hotmail.com>
2023-04-08 18:41:12 +0100

    pass rot13 test #3

--- __tests__/rot13.test.js
+++ __tests__/rot13.test.js
@@ -6,4 +6,16 @@ describe("rot13()", () => {
 
     expect(input).toBe("");
   });
+
+  test("#2 - returns the correctly deciphered string for a single character input", () => {
+    const input = rot13("a");
+
+    expect(input).toBe("n");
+  });
+
+  test("#3 - returns the correctly deciphered string for a multi character input", () => {
+    const input = rot13("ab");
+
+    expect(input).toBe("no");
+  });
 });
--- katas/rot13.js
+++ katas/rot13.js
@@ -1,5 +1,18 @@
 function rot13(str) {
-    return str;
+
+    if (str.length < 1) return str;
+
+    const strCodes = [];
+
+    for (let i = 0; i < str.length; i++) {
+        strCodes.push(str.charCodeAt(i));
+    }
+
+    const rotCodes = strCodes.map((code) => code + 13);
+
+    const decipheredCodes = rotCodes.map((code) => String.fromCharCode(code));
+
+    return decipheredCodes.join("");
 
 }
 
