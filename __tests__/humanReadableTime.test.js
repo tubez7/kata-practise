272bb9323b8687def3d2444c42535156bbe80c47
tubez7 <thescopeboys@hotmail.com>
2023-04-10 18:23:27 +0100

    pass humanReadable test #4

--- __tests__/humanReadableTime.test.js
+++ __tests__/humanReadableTime.test.js
@@ -8,9 +8,23 @@ describe("humanReadable()", () => {
     expect(input).toBe(output);
   });
 
-  test("#2 returns '00:00:01' when argument passed is \"1\"", () => {
+  test("#2 returns '00:00:01' when argument passed is 1", () => {
     const input = humanReadable(1);
     const output = "00:00:01";
+
+    expect(input).toBe(output);
+  });
+
+  test("#3 returns '00:00:59' when argument passed is 59", () => {
+    const input = humanReadable(59);
+    const output = "00:00:59";
+
+    expect(input).toBe(output);
+  });
+
+  test("#4 returns '00:01:00' when argument passed is 60", () => {
+    const input = humanReadable(60);
+    const output = "00:01:00";
 
     expect(input).toBe(output);
   });
--- katas/humanReadableTime.js
+++ katas/humanReadableTime.js
@@ -2,8 +2,9 @@ function humanReadable(seconds) {
   if (!seconds) return "00:00:00";
 
   let secs = 0;
-  let secString = "";
-  let mins = "00";
+  let secString = "00";
+  let mins = 0;
+  let minString = "00";
   let hrs = "00";
 
   while (seconds > 0) {
@@ -14,12 +15,20 @@ function humanReadable(seconds) {
         secString = "0" + secString;
       }
       seconds = 0;
+    } else if (seconds < 3599) {
+      mins += seconds / 60;
+      minString = mins.toString();
+      if (mins < 10) {
+        minString = "0" + minString;
+      }
+      seconds -= 60 * mins;
+      console.log(seconds, "after deduction");
     }
   }
 
-  console.log(`${hrs}:${mins}:${secString}`, "return value");
+  console.log(`${hrs}:${minString}:${secString}`, "return value");
 
-  return `${hrs}:${mins}:${secString}`;
+  return `${hrs}:${minString}:${secString}`;
 }
 
 module.exports = humanReadable;
