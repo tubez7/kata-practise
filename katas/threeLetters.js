ca21451486e3246822007a5c7c35e0d211685a9b
tubez7 <thescopeboys@hotmail.com>
2023-05-04 18:46:54 +0100

    refactor threeLetters by removing redundant code

--- katas/threeLetters.js
+++ katas/threeLetters.js
@@ -1,20 +1,6 @@
 function solution(A, B) {
   let returnStr = "";
 
-  while (B >= 2 * A && A > 0) {
-    returnStr += "bb";
-    returnStr += "a";
-    B -= 2;
-    A--;
-  }
-
-  while (A >= 2 * B && B > 0) {
-    returnStr += "aa";
-    returnStr += "b";
-    A -= 2;
-    B--;
-  }
-
   while (A > 0 || B > 0) {
     if (A > B) {
       if (returnStr.substring(returnStr.length - 2) === "aa") {
@@ -42,6 +28,7 @@ function solution(A, B) {
       }
     }
   }
+  
   return returnStr;
 }
 
