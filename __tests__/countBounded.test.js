7620c316bd46f6f53cea25a1979c9f23eb100ddc
tubez7 <thescopeboys@hotmail.com>
2023-06-07 01:45:37 +0100

    refactor countBounded

--- __tests__/countBounded.test.js
+++ __tests__/countBounded.test.js
@@ -6,4 +6,16 @@ describe("solution()", () => {
 
     expect(solution(2, input)).toBe(9);
   });
+
+  test("function returns 3 from the given example", () => {
+    const input = [3, 5];
+
+    expect(solution(2, input)).toBe(3);
+  });
+
+  test("function returns 10 from the given example", () => {
+    const input = [21, 9, 3, 4, 14, 27, 5, 7];
+
+    expect(solution(2, input)).toBe(10);
+  });
 });
--- katas/countBounded.js
+++ katas/countBounded.js
@@ -1,4 +1,4 @@
-// function solution(K, A) {
+// function solution(K, A) { // original recursive solution
 //   let count = 0;
 
 //   function counter(arr) {
@@ -16,9 +16,41 @@
 //       return counter(arr.slice(1));
 //     }
 //   }
-//   console.log(myMap, "my map");
 
 //   return counter(A);
+// }
+
+
+
+// function solution(K, A) { //attempted solution not using Math global obj
+//   const N = A.length;
+//   let count = 0;
+//   let left = 0;
+//   let right = 0;
+//   let maxVal = A[0];
+//   let minVal = A[0];
+
+//   while (right < N) {    
+//     if (maxVal - minVal <= K) {
+//       count++;
+//       right++;
+//     } else {
+//       left++;
+//       right = left;
+//       minVal = A[right];
+//       maxVal = A[right];
+//     }
+    
+//     maxVal = maxVal < A[right] ? A[right] : maxVal;
+//     minVal = minVal > A[right] ? A[right] : minVal;
+
+//     if (right === N && left < N) {
+//       left++;
+//       right = left;
+//     }
+//   }
+
+//   return count > 1000000000 ? 1000000000 : count;
 // }
 
 function solution(K, A) {
@@ -31,60 +63,26 @@ function solution(K, A) {
 
   while (right < N) {
     if (maxVal - minVal <= K) {
-      console.log(count, "count b4 increase");
       count += right - left + 1;
-      console.log(count, " count after slice checked", left, right, maxVal, minVal);
       right++;
-      
       if (right < N) {
-        maxVal = Math.max(maxVal, A[right]);
-        minVal = Math.min(minVal, A[right]);
+        maxVal = maxVal < A[right] ? A[right] : maxVal;
+        minVal = minVal > A[right] ? A[right] : minVal;
       }
-
     } else {
       if (A[left] === maxVal) {
         maxVal = Math.max(...A.slice(left + 1, right + 1));
       }
+
       if (A[left] === minVal) {
         minVal = Math.min(...A.slice(left + 1, right + 1));
       }
+
       left++;
     }
   }
 
   return count > 1000000000 ? 1000000000 : count;
 }
-
-// function solution(K, A) {
-//     const N = A.length;
-//     let count = 0;
-//     let left = 0;
-//     let right = 0;
-//     let maxVal = A[0];
-//     let minVal = A[0];
-  
-//     while (right < N) {
-//       if (maxVal - minVal <= K) {
-
-//         count += right - left + 1;
-//         // console.log(count, " count after slice checked", left, right, maxVal, minVal);
-//         right++;
-//         if (right < N) {
-//           maxVal = Math.max(maxVal, A[right]);
-//           minVal = Math.min(minVal, A[right]);
-//         }
-//       } else {
-//         if (A[left] === maxVal) {
-//           maxVal = Math.max(...A.slice(left + 1, right + 1));
-//         }
-//         if (A[left] === minVal) {
-//           minVal = Math.min(...A.slice(left + 1, right + 1));
-//         }
-//         left++;
-//       }
-//     }
-  
-//     return count > 1000000000 ? 1000000000 : count;
-// }
 
 module.exports = solution;
