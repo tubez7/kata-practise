e40db5d61d62a11117345b4b1560a650da2474a9
tubez7 <96090739+tubez7@users.noreply.github.com>
2023-08-03 17:03:21 +0100

    update blockLetters boilerplate

---
+++ __tests__/blockLetters.test.js
@@ -0,0 +1,9 @@
+const { describe } = require('node:test');
+const blockLetters = require('../katas/blockLetters.js');
+
+describe('blockLetters', () => {
+  test('returns the correct result for a single character string', () => {
+    const input = blockLetters('a');
+    const output = 'a'; //placeholder
+  });
+});
