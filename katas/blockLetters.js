c8466c3f39fc47fc677603be8092e76135124092
tubez7 <96090739+tubez7@users.noreply.github.com>
2023-08-16 14:23:30 +0100

    start blcokLetters solution for pre-set alpha map

--- katas/blockLetters.js
+++ katas/blockLetters.js
@@ -38,4 +38,27 @@ function blockPrint(input) {
   return blockStr.trimEnd();
 }
 
+// function blockPrint(input) {
+//   const { alpha } = require("../utils/utils.js");
+
+//   const trimmedStr = input.trim();
+//   let blockStr = "";
+
+//   for (let i = 0; i < 7; i++) {
+//     for (let j = 0; j < trimmedStr.length; j++) {
+//       const letter = alpha.get(trimmedStr[i].toLowerCase());
+//       console.log(letter, "letter");
+//       for (let k = 0; letter.length; k++) {
+//         console.log(letter[k], "letter at index k")
+//         blockStr += letter[k] + "\n";
+//         console.log(blockStr, "updated blockStr")
+//       }
+//     }
+//   }
+
+//   console.log(blockStr, "return value");
+//   return blockStr;
+// }
+
+
 module.exports = blockPrint;
