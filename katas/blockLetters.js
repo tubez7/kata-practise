502e6436bce5deffe442722d7d9ee755105b5615
tubez7 <thescopeboys@hotmail.com>
2023-09-05 00:04:05 +0100

    add solution for alterntaive blockPrint Map

--- katas/blockLetters.js
+++ katas/blockLetters.js
@@ -1,4 +1,4 @@
-function blockPrint(input) {
+function blockPrintVersion2(input) {
   const { alphaMap: alpha } = require("../utils/utils.js");
   // imports and renames aphaMap as alpha
 
@@ -35,26 +35,38 @@ function blockPrint(input) {
   return blockString.trimEnd(); // removes potential final space.
 }
 
-// function blockPrint(input) {
-//   const { alpha } = require("../utils/utils.js");
+function blockPrint(input) {
+  const { alpha } = require("../utils/utils.js");
+  // imports apha Map
 
-//   const trimmedStr = input.trim();
-//   let blockStr = "";
+  const trimmedString = input.trim().toLowerCase();
+  // removes leading and trailing whitespace from function argument and converts to lowercase
 
-//   for (let i = 0; i < 7; i++) {
-//     for (let j = 0; j < trimmedStr.length; j++) {
-//       const letter = alpha.get(trimmedStr[i].toLowerCase());
-//       console.log(letter, "letter");
-//       for (let k = 0; letter.length; k++) {
-//         console.log(letter[k], "letter at index k")
-//         blockStr += letter[k] + "\n";
-//         console.log(blockStr, "updated blockStr")
-//       }
-//     }
-//   }
+  let blockString = "";
 
-//   console.log(blockStr, "return value");
-//   return blockStr;
-// }
+  for (let breakCount = 0; breakCount < 7; breakCount++) {
+    // 1st loop iterates each line of the string to return
+    for (let i = 0; i < trimmedString.length; i++) {
+      // 2nd loop iterates over the input string
+      const currentLetter = alpha.get(trimmedString[i]);
+      const stringLine = currentLetter[breakCount];
 
-module.exports = blockPrint;
+      blockString += stringLine;
+
+      if (breakCount < 7) {
+        // adds a space between the characters on the same line
+        blockString += " ";
+      }
+    }
+
+    if (breakCount < 6) {
+      // excluding the last line, trims any potential trailing whitespace after completing a line and appends a line-break to start new line
+      blockString = blockString.trimEnd();
+      blockString += "\n";
+    }
+  }
+
+  return blockString.trimEnd(); // removes potential final space.
+}
+
+module.exports = { blockPrint, blockPrintVersion2 };
