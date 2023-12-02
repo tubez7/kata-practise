80d6bf63f10a5ce45fe79aa06c3c1b8698f5c2de
tubez7 <thescopeboys@hotmail.com>
2023-12-02 19:33:44 +0000

    update progress on landPerimeter kata

--- __tests__/landPerimeter.test.js
+++ __tests__/landPerimeter.test.js
@@ -12,4 +12,22 @@ describe("landPerimeter()", () => {
 
     expect(landPerimeter(input)).toBe(4);
   });
+
+  it("Will return 6 when 2 'X's' in sequence in single grid row", () => {
+    const input = ["OOOOO", "OOOOO", "OOOXX", "OOOOO", "OOOOO"];
+
+    expect(landPerimeter(input)).toBe(6);
+  });
+
+  it("Will return 12 when 2 x 2 'X's' in sequence in a single grid row", () => {
+    const input = ["OOOOO", "OOOOO", "XXOXX", "OOOOO", "OOOOO"];
+
+    expect(landPerimeter(input)).toBe(12);
+  });
+
+  it("Will return correct result for islands across grid rows", () => {
+    const input = ["XXOOO", "OOOOO", "XXOXX", "OOOOO", "OXXXO"];
+
+    expect(landPerimeter(input)).toBe(26);
+  });
 });
