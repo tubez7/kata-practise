06093487eb2a3b338e84da5ee1e2134719981d52
tubez7 <thescopeboys@hotmail.com>
2024-02-12 00:06:47 +0000

    update progress on codewarsRank

--- __tests__/codewarsRank.test.js
+++ __tests__/codewarsRank.test.js
@@ -27,11 +27,41 @@ describe("User", () => {
     });
   });
 
-  describe("Methods of User class", () => {
-    test("calling increaseProgress() method should increase the progress of User class by 3 points when passed a rating equal to the class rank ", () => {
+  describe("incProgress() method of User class when User rank <= activity rank", () => {
+    test("calling increaseProgress() method should increase the progress of User class by 3 points when passed a rating equal to the class rank", () => {
       const testUser = new User();
-      testUser.increaseProgress(-8);
+      testUser.incProgress(-8);
       expect(testUser.progress).toBe(3);
+    });
+
+    test("completing a task with a ranking 1 less than User rank will increase the User rank by a single point", () => {
+      const testUser = new User();
+      testUser.rank = -7;
+      testUser.incProgress(-8);
+      expect(testUser.progress).toBe(1);
+    });
+
+    test("completing a task with a ranking of 2 or more lower than User rank will have no effect on User rank", () => {
+      const testUser = new User();
+      testUser.rank = -6;
+      testUser.incProgress(-8);
+      expect(testUser.progress).toBe(0);
+    });
+
+    test("completing a task with a ranking 1 less than User rank will increase the User rank by a single point when User ranking = 1", () => {
+      const testUser = new User();
+      testUser.rank = 1;
+      testUser.incProgress(-1);
+      expect(testUser.progress).toBe(1);
+    });
+  });
+
+  describe("incProgress() method of User class when User rank < activity rank", () => {
+    test("should increase User rank by 10 points when User rank is 1 lank lower", () => {
+      const testUser = new User();
+      testUser.rank = -1;
+      testUser.incProgress(1);
+      expect(testUser.progress).toBe(10);
     });
   });
 });
--- katas/codewarsRank.js
+++ katas/codewarsRank.js
@@ -2,16 +2,38 @@
 // it must support rank, progress and the incProgress(rank) method
 
 class User {
-    constructor(userId = "New User") {
-        this.userId = userId;
-        this.rank = -8;
-        this.progress = 0;
+  constructor(userId = "New User") {
+    this.userId = userId;
+    this.rank = -8;
+    this.progress = 0;
+  }
+  incProgress(rank) {
+    // function increaseRank() {
+    //   this.rank += Math.floor(this.progress / 100); +1 if 0. If >= 8 = 8;
+    //   this.progress = this.progress % 100; 0 if rank = 8 - ternary operator;
+    // }
+
+    let differenceCorrection = 0;
+
+    if ((this.rank < 0 && rank > 0) || (this.rank > 0 && rank < 0)) {
+      differenceCorrection = 1;
+    } 
+
+    if (rank === this.rank) {
+      this.progress += 3;
+    } else if (this.rank > rank) {
+      if (Math.abs(this.rank - rank) - differenceCorrection === 1) {
+        this.progress += 1;
+      }
+    } else {
+      const difference = Math.abs(rank - this.rank) - differenceCorrection;
+      this.progress += 10 * difference * difference;
     }
-    increaseProgress(rank) {
-        if (rank === this.rank) {
-            this.progress += 3;
-        }
-    }
+
+    // if (this.progress >= 100) {
+    //     increaseRank();
+    //  }
+  }
 }
 
 module.exports = User;
